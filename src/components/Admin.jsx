add9514e3499920beb7f7412f021df97727e93ab
Vansh6401 <Vanshsharma6401@gmail.com>
2025-07-21 00:57:01 +0530

    update

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
+import "./Admin.css";
 export default function Admin() {
   return (
-    <div>
+    <div className="admin-container">
       <Link to="/admin">Users</Link>-
       <Link to="/admin/products">Products</Link>-
       <Link to="/admin/orders">Orders</Link>
