8eb8e657498b349267d0315ab5bd5f8a606fcb5b
Vansh6401 <Vanshsharma6401@gmail.com>
2025-07-23 14:40:29 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -17,17 +17,17 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "/icons/logo.png",
+            src: "/icons/sun-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icons/logo.png",
+            src: "/icons/sun-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "/icons/logo.png",
+            src: "/icons/sun-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
